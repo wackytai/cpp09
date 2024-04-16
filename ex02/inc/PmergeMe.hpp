d487520a3ca6bfa2e7b79b012d68c8caecc118d7
wackytai <tainahmonteiro.info@gmail.com>
2024-04-16 15:57:31 +0100

    ex02 working

--- ex02/inc/PmergeMe.hpp
+++ ex02/inc/PmergeMe.hpp
@@ -13,20 +13,22 @@
 class PmergeMe
 {
 	private:
-		std::vector<int> _seq;
+		std::vector<int>	_seq;
+		static int64_t		groups[];
 
 	public:
 		PmergeMe();
 		PmergeMe(const PmergeMe &object);
 		~PmergeMe();
-		PmergeMe &operator=(const PmergeMe &object);
-		bool validateInput(char **input);
-		void mergeInsertion( void );
-		void inPairComparison( void );
-		void orderPairs( std::vector<std::pair<int, int> > pairs );
-		static bool compare( const std::pair<int,int> &a, const std::pair<int,int> &b );
-		void dividePairs( void );
-		std::vector<int> merge( std::vector<int> &main, std::vector<int> &left );
+		PmergeMe			&operator=(const PmergeMe &object);
+		bool 				validateInput(char **input);
+		void 				mergeInsertion( void );
+		void 				inPairComparison( void );
+		void 				orderPairs( std::vector<std::pair<int, int> > pairs );
+		static bool 		compare( const std::pair<int,int> &a, const std::pair<int,int> &b );
+		void 				dividePairs( void );
+		std::vector<int> 	merge( std::vector<int> &main, std::vector<int> &left );
+		void				check_sorted(std::vector<int> final);
 		class InvalidInputException : public std::exception
 		{
 			public:
--- ex02/src/PmergeMe.cpp
+++ ex02/src/PmergeMe.cpp
@@ -1,5 +1,20 @@
 #include "../inc/PmergeMe.hpp"
 
+int64_t	PmergeMe::groups[] =
+{
+	2u, 2u, 6u, 10u, 22u, 42u, 86u, 170u, 342u, 682u, 1366u,
+    2730u, 5462u, 10922u, 21846u, 43690u, 87382u, 174762u, 349526u, 699050u,
+    1398102u, 2796202u, 5592406u, 11184810u, 22369622u, 44739242u, 89478486u,
+    178956970u, 357913942u, 715827882u, 1431655766u, 2863311530u, 5726623062u,
+    11453246122u, 22906492246u, 45812984490u, 91625968982u, 183251937962u,
+    366503875926u, 733007751850u, 1466015503702u, 2932031007402u, 5864062014806u,
+    11728124029610u, 23456248059222u, 46912496118442u, 93824992236886u, 187649984473770u,
+    375299968947542u, 750599937895082u, 1501199875790165u, 3002399751580331u,
+    6004799503160661u, 12009599006321322u, 24019198012642644u, 48038396025285288u,
+    96076792050570576u, 192153584101141152u, 384307168202282304u, 768614336404564608u,
+    1537228672809129216u, 3074457345618258432u, 6148914691236516864u
+};
+
 PmergeMe::PmergeMe() {}
 
 PmergeMe::PmergeMe(const PmergeMe &object)
@@ -161,9 +176,10 @@ void PmergeMe::dividePairs( void )
 		std::cout << *it << " ";
 	std::cout << std::endl;
 	_seq = merge(main, left);
+	check_sorted(_seq);
 }
 
-std::vector<int> PmergeMe::merge( std::vector<int> &main, std::vector<int> &left )
+/* std::vector<int> PmergeMe::merge( std::vector<int> &main, std::vector<int> &left )
 {
 	std::vector<int> g;
 
@@ -199,9 +215,9 @@ std::vector<int> PmergeMe::merge( std::vector<int> &main, std::vector<int> &left
 			++itG;
 	    if (it1 != left.end() && itG != g.end())
 		{
-			/* it = left.begin();
-	        std::advance(it, std::distance(it, it1) + 1);
-	        it1 = it - 1; */
+			//it = left.begin();
+	        //std::advance(it, std::distance(it, it1) + 1);
+	        //it1 = it - 1;
 			//std::advance(it, (std::accumulate(g.begin(), (itG + 1), 0) - 1));
 			it1 += (std::accumulate(g.begin(), itG, 0) - 1);
 			it = it1 + *itG;
@@ -216,6 +232,49 @@ std::vector<int> PmergeMe::merge( std::vector<int> &main, std::vector<int> &left
 		std::cout << *it << " ";
 	std::cout << std::endl;
 	return main;
+} */
+
+std::vector<int> PmergeMe::merge( std::vector<int> &main, std::vector<int> &left )
+{
+	std::vector<int>::iterator f = left.end() - 1;
+	std::vector<int>::iterator b = left.begin();
+	std::vector<int>::iterator e;
+
+
+	for (int i = 0; b <= f; i++)
+	{
+		e = (groups[i] <= std::distance(b, f)) ? b + groups[i] - 1 : f;
+		for (int j = 0; j <= groups[i]; j++)
+		{
+			main.insert(std::upper_bound(main.begin(), main.end(), *e), *e);
+			if (e == b)
+				break ;
+			e--;
+		}
+		std::advance(b, (groups[i]));
+	}
+
+	std::cout << "After: ";
+	for (std::vector<int>::iterator it = main.begin(); it != main.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	return main;
+}
+
+void	PmergeMe::check_sorted(std::vector<int> final)
+{
+	std::vector<int>::iterator	begin = ++final.begin();
+	std::vector<int>::iterator	end = final.end();
+
+	for (; begin != end; begin++)
+	{
+		if (*begin < *(begin - 1))
+		{
+			std::cout << "NOT SORTED\n";
+			return;
+		}
+	}
+	std::cout << "SORTED\n";
 }
 
 const char *PmergeMe::InvalidInputException::what() const throw()
